750b8239ed803a24486f2bfc3b739840eeed1ee9
Kyler Johnson <kyler.johnson.01@gmail.com>
2022-02-21 11:47:57 -0800

    Implement dynamic tapping term

--- keyboards/splitkb/kyria/keymaps/hpickles/rules.mk
+++ keyboards/splitkb/kyria/keymaps/hpickles/rules.mk
@@ -2,3 +2,4 @@ OLED_ENABLE = no
 OLED_DRIVER = SSD1306   # Enables the use of OLED displays
 ENCODER_ENABLE = no       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
+DYNAMIC_TAPPING_TERM_ENABLE = yes
