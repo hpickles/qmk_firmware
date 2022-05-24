e989f945a28b7cffffa7ba7803c5f7557450f3c7
Kyler Johnson <kyler.johnson.01@gmail.com>
2022-05-24 10:15:31 -0700

    Add initial combo setup

--- keyboards/splitkb/kyria/keymaps/hpickles/keymap.c
+++ keyboards/splitkb/kyria/keymaps/hpickles/keymap.c
@@ -25,6 +25,24 @@ enum layers {
     _ADJUST,
 };
 
+enum combos {
+  AB_ESC,
+  JK_TAB,
+  QW_SFT,
+  COMBO_LENGTH,
+};
+
+uint16_t COMBO_LEN = COMBO_LENGTH;
+
+const uint16_t PROGMEM ab_combo[] = {KC_A, KC_B, COMBO_END};
+const uint16_t PROGMEM jk_combo[] = {KC_J, KC_K, COMBO_END};
+const uint16_t PROGMEM qw_combo[] = {KC_Q, KC_W, COMBO_END};
+
+combo_t key_combos[] = {
+  [AB_ESC] = COMBO(ab_combo, KC_ESC),
+  [JK_TAB] = COMBO(jk_combo, KC_TAB),
+  [QW_SFT] = COMBO(qw_combo, KC_LSFT),
+};
 
 // Aliases for readability
 #define QWERTY   DF(_QWERTY)
